eb1b4ce165f934f2b2a6b647a55adc7cfcdc8b31
edward <leonardo071123@gmail.com>
2018-07-10 20:49:39 +0800

    add admin

---
+++ admin/manager.php
@@ -0,0 +1,152 @@
+<?php
+	include "../includes/init.php";
+    include "../includes/_inc.php";
+
+    $sql = "SELECT * FROM manager WHERE 1  ORDER BY id DESC";
+    
+    $lists = $db->rawQuery($sql);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>寶台建設後台</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+       <?php  include('menu.php');?>
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            <header class="header-desktop">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="header-wrap">
+                            經手人列表
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- END HEADER DESKTOP-->
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row m-t-30">
+                            <div class="col-md-12">
+                                <!-- DATA TABLE-->
+                                <div class="table-responsive m-b-40">
+                                    <table class="table table-borderless table-data2">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>姓名</th>
+                                                <th>動作</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                             <?php
+                                                $i = 1;
+                                                foreach ($lists as $list){ 
+                                                    echo '<tr>';
+                                                    echo '<td>'.$i.'</td>';
+                                                    echo '<td>'.$list['name'].'</td>';
+                                                    echo '<td><a href="editmanager.php?id='.$list['id'].'">修改</a>/<a href="#" id="del_'.$list['name'].'" class="del_one">刪除</a></td>';
+                                                    echo '</tr>';
+                                                    $i++;
+                                                }
+                                            ?>
+                                            </tbody>
+                                    </table>
+                                </div>
+                                <!-- END DATA TABLE-->
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="vendor/slick/slick.min.js">
+    </script>
+    <script src="vendor/wow/wow.min.js"></script>
+    <script src="vendor/animsition/animsition.min.js"></script>
+    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
+    <script type="text/javascript">
+    $(document).on("click", ".del_one", function() {
+        var obj = {};
+        obj['id'] = $(this).prop("id").split("_")[1];
+        $.ajax({
+            url: '../delete_manager.php',
+            cache: false,
+            dataType: 'html',
+            type: 'POST',
+            data: obj,
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log('HTTP status code: ' + jqXHR.status + '\n' +
+                'textStatus: ' + textStatus + '\n' +
+                'errorThrown: ' + errorThrown);
+                console.log('HTTP message body (jqXHR.responseText): ' + '\n' + jqXHR.responseText);
+            },
+            success: function(response) {
+                location.reload();
+            }
+        });
+    });
+    </script>
+</body>
+
+</html>
+<!-- end document-->
---
+++ admin/menu.php
@@ -0,0 +1,149 @@
+ <!-- HEADER MOBILE-->
+        <header class="header-mobile d-block d-lg-none">
+            <div class="header-mobile__bar">
+                <div class="container-fluid">
+                    <div class="header-mobile-inner">
+                        <a class="logo" href="index.html">
+                            寶台建設後台
+                        </a>
+                        <button class="hamburger hamburger--slider" type="button">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <nav class="navbar-mobile">
+                <div class="container-fluid">
+                    <ul class="navbar-mobile__list list-unstyled">
+                          <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>建案</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="addproject.php">新增</a>
+                                </li>
+                                <li>
+                                    <a href="project.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>經手人</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="addmanager.php">新增</a>
+                                </li>
+                                <li>
+                                    <a href="manager.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                         <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>客戶會員</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="customs.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                       <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>來訪記錄</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="visits.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                         <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>客服紀錄</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                  <li>
+                                    <a href="addservice.php">新增</a>
+                                </li>
+                                <li>
+                                    <a href="service.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- END HEADER MOBILE-->
+
+        <!-- MENU SIDEBAR-->
+        <aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                    寶台建設後台
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>建案</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="addproject.php">新增</a>
+                                </li>
+                                  <li>
+                                    <a href="project.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                         <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>經手人</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                 <li>
+                                    <a href="addmanager.php">新增</a>
+                                </li>
+                                <li>
+                                    <a href="manager.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                         <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>客戶會員</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="customs.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                       <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>來訪記錄</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="visits.php">列表</a>
+                                </li>
+                            </ul>
+                        </li>
+                         <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>客服紀錄</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                  <li>
+                                    <a href="addservice.php">新增</a>
+                                </li>
+                                <li>
+                                    <a href="service.php">列表</a>
+                                </li>
+                               
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <!-- END MENU SIDEBAR-->
