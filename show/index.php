5e97b3421d41f8652fbccd28715718ccc71b6af7
edward <leonardo071123@gmail.com>
2018-07-23 11:42:48 +0800

    add front page "show"

---
+++ show/index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>明日禾禾</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="3房自備48萬起交屋，桃園美術館特區，A19站旁，面3000坪公園，吹吹風綠生活，老街溪河濱單車樂活" />
+	<meta name="keywords" content="明日禾禾, 首付48萬, 桃園美術館特區, A19站, 3000坪公園, 綠意環抱, 青埔三大萬坪公園, 美術館, 限量" />
+	<meta name="author" content="明日禾禾" />
+
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content="明日禾禾"/>
+	<meta property="og:description" content="3房自備48萬起交屋，桃園美術館特區，A19站旁，面3000坪公園"/>
+	<meta name="twitter:title" content="明日禾禾" />
+    
+    <!-- 16x16 ICON -->
+    <link rel="Shortcut Icon" type="image/x-icon" href="images/Icon.png"/>
+    
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Prata" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Style -->
+	<link rel="stylesheet" href="css/style.css">
+    
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+     <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PGHHD76');</script>
+    <!-- End Google Tag Manager -->
+   
+
+
+	
+    </head>
+	<body style="padding-top: 0px; background-color: #ECBFB7;">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGHHD76"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+
+
+	
+<section id="0" class="navbar-fixed-top col-lg-1 col-sm-1 col-xs-2">
+<div class="row">
+<a href="tel:03-2870222"><img src="images/ICONOP-01.png" class="img-rounded img-responsive visible-lg visible-md" alt="Placeholder image"></a>
+<a href="index.html#projects01"><img src="images/ICONOP-03.png" class="img-rounded img-responsive visible-lg visible-md" alt="Placeholder image"></a>
+<a href="tel:03-2870222" target="_blank" onclick="ga(&#39;send&#39;, &#39;event&#39;, &#39;button&#39;, &#39;clicktocall&#39;)"><img src="images/ICONOP-01.png" class="img-rounded img-responsive visible-sm visible-xs" alt="Placeholder image"></a>
+<a href="index.html#projects01" target="_blank"><img src="images/ICONOP-03.png" class="img-rounded img-responsive visible-sm visible-xs" alt="Placeholder image"></a>
+<a href="https://www.google.com.tw/maps/place/%E6%98%8E%E6%97%A5%E6%9C%97%E6%9C%97%EF%BC%8E%E6%98%8E%E6%97%A5%E7%A6%BE%E7%A6%BE%E6%8E%A5%E5%BE%85%E6%9C%83%E9%A4%A8/@25.010022,121.2027492,17z/data=!4m12!1m6!3m5!1s0x346821c43c1b01c5:0x25501cbb8ed9ac97!2z5piO5pel5pyX5pyX77yO5piO5pel56a-56a-5o6l5b6F5pyD6aSo!8m2!3d25.010022!4d121.2049379!3m4!1s0x346821c43c1b01c5:0x25501cbb8ed9ac97!8m2!3d25.010022!4d121.2049379" target="_blank"><img src="images/ICONOP-02.png" class="img-rounded img-responsive visible-lg visible-md" alt="Placeholder image"></a>
+<a href="https://www.google.com.tw/maps/place/%E6%98%8E%E6%97%A5%E6%9C%97%E6%9C%97%EF%BC%8E%E6%98%8E%E6%97%A5%E7%A6%BE%E7%A6%BE%E6%8E%A5%E5%BE%85%E6%9C%83%E9%A4%A8/@25.010022,121.2027492,17z/data=!4m12!1m6!3m5!1s0x346821c43c1b01c5:0x25501cbb8ed9ac97!2z5piO5pel5pyX5pyX77yO5piO5pel56a-56a-5o6l5b6F5pyD6aSo!8m2!3d25.010022!4d121.2049379!3m4!1s0x346821c43c1b01c5:0x25501cbb8ed9ac97!8m2!3d25.010022!4d121.2049379" target="_blank"><img src="images/ICONOP-02.png" class="img-rounded img-responsive visible-sm visible-xs" alt="Placeholder image"></a>
+  </div>
+</section>
+<!--<section id="fh5co-services" data-section="about">-->
+<!--<div class="fh5co-services" data-section="there">-->
+    <div class="container">
+    <img src="images/kk.jpg" class="img-responsive" alt="明日禾禾3房自備48萬起交屋">
+    <!-- <img src="images/KV01.jpg" class="img-responsive" alt="明日禾禾3房自備48萬起交屋">
+    <img src="images/NEW.jpg" class="img-responsive" alt="台北規格A19站公園宅">
+    <img src="images/KV03.jpg" class="img-responsive" alt="3000大公園面對面">
+    <img src="images/KV04.jpg" class="img-responsive" alt="萬坪青塘園 吹吹風綠生活">
+    <img src="images/KV05.jpg" class="img-responsive" alt="美術館國際競圖出爐">
+    <img src="images/KV06.jpg" class="img-responsive" alt="青埔最大 全聯福利社1分鐘">
+    <img src="images/KV07.jpg" class="img-responsive" alt="機捷A19站，環遊世界隨時啟航">
+    <img src="images/KV08.jpg" class="img-responsive" alt="70億環球購物中心">
+    <img src="images/KV09.jpg" class="img-responsive" alt="大園國際雙語高中">
+    <img src="images/KV10.jpg" class="img-responsive" alt="桃園國際棒球場">
+    <img src="images/KV11.jpg" class="img-responsive" alt="禮賓大廳尊榮美學">
+    <img src="images/KV12.jpg" class="img-responsive" alt="健身館活力泉源">
+        <img src="images/new2.jpg" class="img-responsive" alt="尊榮3房 三代同堂不是夢想"> -->
+</div>
+		
+
+        
+  
+  
+   
+
+
+<div class="fh5co-services" data-section="tw">
+
+
+
+<div class="fh5co-services" data-section="contact">
+<!-- 錨點 -->
+<div id="projects01"></div>
+
+<section id="fh5co-footer" data-section="contact" role="contentinfo">
+	<div class="container">
+			<div class="row">
+				<div class="col-md-4 to-animate">
+                    <hr>
+					<h3><b><span style="color:#000000">明日禾禾</span></b></h3>
+					<p>桃園市中壢區高鐵特區<br>3房自備48萬起交屋<br>桃園美術館特區，<br>A19站旁，面3000坪公園<br>吹吹風綠生活，老街溪河濱單車樂活</p>
+                    <hr>
+                     <h3><b><span style="color:#000000">連接資訊</span></b></h3>
+					<ul class="social-media">
+					  <li><a href="https://www.facebook.com/hiyes.tw/?hc_ref=ARSxhFH88YSA1YE4f-AE8Scdebyfln6Dy7Yl54J-Ly_fYh1dOKCC8DH96ozdLK9m9TE&fref=nf"><img src="images/FB.png" class="img-responsive" alt="Placeholder image"></a></li>
+                      <li><a href="https://line.me/R/ti/p/########" target="_blank" class="facebook"><img src="images/line2.png" class="img-responsive" alt="Placeholder image"></a></li>
+						
+					</ul>
+				</div>
+
+				<div class="col-md-4 to-animate">
+                    <hr>
+					<h3><b><span style="color:#000000">建案資訊</span></b></h3>
+					
+                    <ul class="contact-info" style="text-align: left; font-weight: 300; margin-left: 60px;">
+						
+                        <li>投資興建 ： 新潤興業股份有限公司</li>
+                        <li>廣告代銷 ： 海悅國際開發股份有限公司</li>
+                        <li>建築規劃 ： 郭長庚建築師</li>
+						<li>結構顧問 ： 宋治中顧問-宏盛帝寶結構顧問</li>
+                        <li>公設規劃 ： 趙東洲-Idea-Tops最佳藝術設計獎</li>
+                        <li>園藝景觀 ： 郭長庚-台灣建築設計獎</li>
+                        <li>地段號碼 ： 青芝段96地號</li>
+                        <li>基地面積 ： 525坪</li>
+                        <li>建築樓高 ： 地上14層，地下4層</li>
+                        <li>規劃產品 ： 2-4房</li>
+                        <li>聯絡電話 ： 03-2870222</li>
+					</ul>    
+</div>
+
+				<div class="col-md-4 to-animate">
+                   <hr>
+					<h3><b><span style="color:#000000">預約賞屋</span></b></h3>
+					<form class="contact-form"  method="post" action="userLog.php#fh5co-footer">
+						<div class="form-group">
+							<label for="userName" class="sr-only"><small class="text-danger required">*</small>姓名</label>
+							<input type="text" class="form-control" id="userName" name="userName" minlength="1" maxlength="30" placeholder="姓名" required="required">
+						</div>
+						<div class="form-group">
+							<label for="phone" class="sr-only"><small class="text-danger required">*</small>連絡手機</label>
+							<input type="tel" class="form-control" id="phone" name="phone" pattern="[0-9]{10}" minlength="10" maxlength="10" placeholder="連絡手機" required="required">
+						</div>
+						<div class="form-group">
+							<label for="message" class="sr-only">Message</label>
+							<textarea class="form-control" id="message" name="message" rows="7" minlength="0" maxlength="299" placeholder="300字內的敘述"></textarea>
+						</div>
+                        <div class="form-group">
+                            <input type="submit" id="btn-submit" class="btn btn-send-message btn-md" onclick="window.open('./thank.html ', 'mansion', config='height=500,width=500');" value="送出預約">
+						</div>
+					</form>
+				</div>
+			</div>
+  </div>
+	</section>
+    <div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.708418104198!2d121.20274921537525!3d25.010021983983602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346821c43c1b01c5%3A0x25501cbb8ed9ac97!2z5piO5pel5pyX5pyX77yO5piO5pel56a-56a-5o6l5b6F5pyD6aSo!5e0!3m2!1szh-TW!2stw!4v1528959032742" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+
+	
+	<!-- jQuery -->
+	<!-- <script src="js/jquery.min.js"></script> -->
+	<script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	
+	<script src="js/google_map.js"></script>
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+ </div></div>
+</body>
+</html>
+
